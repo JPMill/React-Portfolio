593dfc3532b3c4c5caffd6fd8291486b1b633e4e
Justin Miller <justinmiller617@gmail.com>
2025-02-03 17:54:57 -0600

    updated projects in portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -10,18 +10,6 @@ import './Portfolio.css'
 function Portfolio() {
   const projects = [
     {
-      title: 'Vehicle Builder',
-      imageUrl: Image1,
-      githubUrl: 'https://github.com/JPMill/Vehicle-Builder',
-      description: 'This project uses TypeScript that allows you to interact with the CLI to create your own car, truck or motorbike'
-    },
-    {
-      title: 'Professional README Generator',
-      imageUrl: Image2,
-      githubUrl: 'https://github.com/JPMill/README-Generator',
-      description: 'This project is a Node.js application that dynamically generates a README.md file for a project based on user input.'
-    },
-    {
       title: 'E-Planner',
       imageUrl: Image3,
       deployedUrl: 'https://ejmaller7.github.io/E-Planner/',
@@ -34,14 +22,7 @@ function Portfolio() {
       deployedUrl: 'https://null-labs-frontend.onrender.com/',
       githubUrl: 'https://github.com/ejmaller7/Null-Labs',
       description: 'Our team worked togeher to develop a full-stack application that leverages the MERN stack. This application is a digital video game store that allows users to get information on a variety of games via an api call. Users can create an account and add game deals to their wishlist/shopping cart.'
-    },
-    {
-      title: 'Weather Dashboard',
-      imageUrl: Image5,
-      deployedUrl: 'https://weather-dashboard-3-2e7v.onrender.com/',
-      githubUrl: 'https://github.com/JPMill/Weather-Dashboard',   
-      description: 'This is a Full-Stack project the pulls data from OpenWeatherMap api and displays the weather and forecast for a city that is searched for.'
-    },
+    }
   ]
 
   return (
