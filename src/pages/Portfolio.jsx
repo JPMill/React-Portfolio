efe863f7870e52ea77c527274e17469ecbf33cb0
Justin Miller <justinmiller617@gmail.com>
2025-02-26 20:39:10 -0600

    Added current project

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,11 +4,18 @@ import Image1 from '../assets/images/vehiclebuilder.png';
 import Image2 from '../assets/images/readme.png';
 import Image3 from '../assets/images/eplanner.png';
 import Image4 from '../assets/images/NullLabsLogo3.png'
-import Image5 from '../assets/images/WeatherApi.png'
+import Image5 from '../assets/images/DEV.CONNECT.png'
 import './Portfolio.css'
 
 function Portfolio() {
   const projects = [
+    {
+      title: 'Dev.Connect',
+      imageUrl: Image5,
+      deployedUrl: 'https://dev-connect-1-eiz8.onrender.com/',
+      githubUrl: 'https://github.com/ejmaller7/Dev.connect',
+      description: 'Dev.Connect is a MERN-stack networking platform for developers to connect, showcase projects, and explore job opportunities. It features user profiles, community feed, job listings, and industry news updates. Development is ongoing.'
+    },
     {
       title: 'Null Labs',
       imageUrl: Image4,
