c646cec31de704a27d831095289af7a357f2a927
Justin Miller <justinmiller617@gmail.com>
2025-02-27 14:40:55 -0600

    updated about me info

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -6,7 +6,8 @@ function AboutMe() {
   return (
     <section className="about-me">
       <img src={profile} alt="Justin Miller" className="profile-pic" />
-      <p>Hello! I'm Justin, a software engineering student with a passion for web development.</p>
+      <h3>Hello! I'm Justin Miller</h3>
+      <p>I'm a student in Rice University's BootCamp for software development, currently pursuing an Associate of Science in Computer Science. I have a passion for software engineering and am excited to explore the world of tech, particularly with plans to eventually earn a Bachelor's in Cybersecurity. I’m eager to pursue opportunities in software development and innovation.</p>
     </section>
   );
 }
